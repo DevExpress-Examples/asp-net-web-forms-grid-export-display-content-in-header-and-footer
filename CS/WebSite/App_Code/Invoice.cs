7fb02de2571b20ce82e41cb5cb1479f054f0dd91
khlopova.ekaterina <ekaterina.khlopova@devexpress.com>
2018-08-30 08:27:29 +0300

    Access database removed, ObjectDataSource and in-memory datd used instead

---
+++ CS/WebSite/App_Code/Invoice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class Invoice : IEquatable<Invoice> {
+    public int Id { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+
+    public Invoice(int id, string description, decimal price) {
+        Id = id;
+        Description = description;
+        Price = price;
+    }
+
+    public Invoice() { }
+
+    public bool Equals(Invoice other) {
+        return this.Id.Equals(other.Id);
+    }
+}
