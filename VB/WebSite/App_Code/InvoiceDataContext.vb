ba7c6f1b42e37a5a734f06fa856aeec5e3f55a2a
DevExpressExampleBot <fakeEmail>
2018-08-30 09:37:47 +0300

    Source auto update [skip ci]

---
+++ VB/WebSite/App_Code/InvoiceDataContext.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Web
+Imports System.Linq
+
+<DataObject(True)> _
+Public Class InvoiceDataContext
+    Private Property Invoices() As List(Of Invoice)
+        Get
+            If HttpContext.Current.Session("Invoices") Is Nothing Then
+                Dim list As New List(Of Invoice)()
+
+                list.Add(New Invoice(0, "Invoice1", 10.0D))
+                list.Add(New Invoice(1, "Invoice2", 15.0D))
+                list.Add(New Invoice(2, "Invoice3", 20.0D))
+                HttpContext.Current.Session("Invoices") = list
+                Return list
+            Else
+                Return CType(HttpContext.Current.Session("Invoices"), List(Of Invoice))
+            End If
+        End Get
+        Set(ByVal value As List(Of Invoice))
+            HttpContext.Current.Session("Invoices") = value
+        End Set
+    End Property
+
+    <DataObjectMethod(DataObjectMethodType.Select, True)> _
+    Public Function GetInvoices() As List(Of Invoice)
+        Return Invoices
+    End Function
+End Class
--- VB/WebSite/Default.aspx.vb
+++ VB/WebSite/Default.aspx.vb
@@ -7,10 +7,9 @@ Imports System.Drawing
 Partial Public Class _Default
     Inherits System.Web.UI.Page
 
-    Private headerImage As System.Drawing.Image
+    Private headerImage As Image
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
-
     End Sub
     Protected Sub pdf_Click(ByVal sender As Object, ByVal e As EventArgs)
         Export("pdf")
@@ -23,48 +22,50 @@ Partial Public Class _Default
     End Sub
 
     Private Sub Export(ByVal format As String)
-        Dim ps As New PrintingSystem()
+        Dim ps As New PrintingSystemBase()
 
-        Me.headerImage = System.Drawing.Image.FromFile(Server.MapPath("~\Images\pix.png"))
-        Using Me.headerImage
-            Dim header As New Link()
-            AddHandler header.CreateDetailArea, AddressOf header_CreateDetailArea
+        headerImage = Image.FromFile(Server.MapPath("~/Images/DevExpress-Logo-Large-Color.png"))
+        Using headerImage
+            Dim header As New LinkBase()
+            AddHandler header.CreateDetailHeaderArea, AddressOf Header_CreateDetailHeaderArea
 
-            Dim link1 As New PrintableComponentLink()
+            Dim link1 As New PrintableComponentLinkBase()
             link1.Component = Me.ASPxGridViewExporter1
-
-            Dim compositeLink As New CompositeLink(ps)
+            AddHandler link1.CreateReportFooterArea, AddressOf Link1_CreateReportFooterArea
+            Dim compositeLink As New CompositeLinkBase(ps)
             compositeLink.Links.AddRange(New Object() { header, link1 })
 
             compositeLink.CreateDocument()
             Using stream As New MemoryStream()
                 Select Case format
                     Case "xls"
-                        compositeLink.PrintingSystem.ExportToXls(stream)
+                        compositeLink.ExportToXls(stream)
                         WriteToResponse("filename", True, format, stream)
                     Case "pdf"
-                        compositeLink.PrintingSystem.ExportToPdf(stream)
+                        compositeLink.ExportToPdf(stream)
                         WriteToResponse("filename", True, format, stream)
                     Case "rtf"
-                        compositeLink.PrintingSystem.ExportToRtf(stream)
+                        compositeLink.ExportToRtf(stream)
                         WriteToResponse("filename", True, format, stream)
                     Case Else
                 End Select
-
             End Using
             ps.Dispose()
         End Using
     End Sub
-
-    Private Sub header_CreateDetailArea(ByVal sender As Object, ByVal e As CreateAreaEventArgs)
+    Private Sub Header_CreateDetailHeaderArea(ByVal sender As Object, ByVal e As CreateAreaEventArgs)
         e.Graph.BorderWidth = 0
-
         Dim r As New Rectangle(0, 0, headerImage.Width, headerImage.Height)
         e.Graph.DrawImage(headerImage, r)
-
-        r = New Rectangle(0, headerImage.Height, 400, 50)
+        r = New Rectangle(0, headerImage.Height, 200, 50)
         e.Graph.DrawString("Additional Header information here....", r)
-
+    End Sub
+    Private Sub Link1_CreateReportFooterArea(ByVal sender As Object, ByVal e As CreateAreaEventArgs)
+        e.Graph.BorderWidth = 0
+        Dim r As New Rectangle(0, 20, 200, 50)
+        e.Graph.Font = New Font("Times New Roman", 12, FontStyle.Italic)
+        e.Graph.ForeColor = Color.Gray
+        e.Graph.DrawString("This is footer", r)
     End Sub
 
     Private Sub WriteToResponse(ByVal fileName As String, ByVal saveAsFile As Boolean, ByVal fileFormat As String, ByVal stream As MemoryStream)
