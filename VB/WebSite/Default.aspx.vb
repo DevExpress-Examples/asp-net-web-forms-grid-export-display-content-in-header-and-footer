9453c44c9df09c22666e7320d8e1643ef0dfee91
DevExpressExampleBot <fakeEmail>
2018-08-30 11:29:28 +0300

    Source auto update [skip ci]

--- VB/WebSite/Default.aspx.vb
+++ VB/WebSite/Default.aspx.vb
@@ -7,44 +7,66 @@ Imports System.Drawing
 Partial Public Class _Default
     Inherits System.Web.UI.Page
 
-    Private headerImage As System.Drawing.Image
+    Private headerImage As Image
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+    End Sub
+    Protected Sub pdf_Click(ByVal sender As Object, ByVal e As EventArgs)
+        Export("pdf")
+    End Sub
+    Protected Sub rtf_Click(ByVal sender As Object, ByVal e As EventArgs)
+        Export("rtf")
+    End Sub
+    Protected Sub xls_Click(ByVal sender As Object, ByVal e As EventArgs)
+        Export("xls")
 
     End Sub
-    Protected Sub b1_Click(ByVal sender As Object, ByVal e As EventArgs)
-        Dim ps As New PrintingSystem()
 
-        Me.headerImage = System.Drawing.Image.FromFile(Server.MapPath("~\Images\pix.png"))
-        Using Me.headerImage
+    Private Sub Export(ByVal format As String)
+        Dim ps As New PrintingSystemBase()
 
-            Dim header_Renamed As New Link()
-            AddHandler header_Renamed.CreateDetailArea, AddressOf header_CreateDetailArea
+        headerImage = Image.FromFile(Server.MapPath("~/Images/DevExpress-Logo-Large-Color.png"))
+        Using headerImage
+            Dim header As New LinkBase()
+            AddHandler header.CreateDetailHeaderArea, AddressOf Header_CreateDetailHeaderArea
 
-            Dim link1 As New DevExpress.Web.ASPxGridView.Export.Helper.GridViewLink(Me.ASPxGridViewExporter1)
-
-            Dim compositeLink As New CompositeLink(ps)
-            compositeLink.Links.AddRange(New Object() { header_Renamed, link1 })
+            Dim link1 As New PrintableComponentLinkBase()
+            link1.Component = ASPxGridViewExporter1
+            AddHandler link1.CreateReportFooterArea, AddressOf Link1_CreateReportFooterArea
+            Dim compositeLink As New CompositeLinkBase(ps)
+            compositeLink.Links.AddRange(New Object() { header, link1 })
 
             compositeLink.CreateDocument()
             Using stream As New MemoryStream()
-                compositeLink.PrintingSystem.ExportToPdf(stream)
-                WriteToResponse("filename", True, "pdf", stream)
+                Select Case format
+                    Case "xls"
+                        compositeLink.ExportToXls(stream)
+                        WriteToResponse("filename", True, format, stream)
+                    Case "pdf"
+                        compositeLink.ExportToPdf(stream)
+                        WriteToResponse("filename", True, format, stream)
+                    Case "rtf"
+                        compositeLink.ExportToRtf(stream)
+                        WriteToResponse("filename", True, format, stream)
+                    Case Else
+                End Select
             End Using
             ps.Dispose()
-
         End Using
     End Sub
-
-    Private Sub header_CreateDetailArea(ByVal sender As Object, ByVal e As CreateAreaEventArgs)
+    Private Sub Header_CreateDetailHeaderArea(ByVal sender As Object, ByVal e As CreateAreaEventArgs)
         e.Graph.BorderWidth = 0
-
         Dim r As New Rectangle(0, 0, headerImage.Width, headerImage.Height)
         e.Graph.DrawImage(headerImage, r)
-
-        r = New Rectangle(0, headerImage.Height, 400, 50)
+        r = New Rectangle(0, headerImage.Height, 200, 50)
         e.Graph.DrawString("Additional Header information here....", r)
-
+    End Sub
+    Private Sub Link1_CreateReportFooterArea(ByVal sender As Object, ByVal e As CreateAreaEventArgs)
+        e.Graph.BorderWidth = 0
+        Dim r As New Rectangle(0, 20, 200, 50)
+        e.Graph.Font = New Font("Times New Roman", 12, FontStyle.Italic)
+        e.Graph.ForeColor = Color.Gray
+        e.Graph.DrawString("This is footer", r)
     End Sub
 
     Private Sub WriteToResponse(ByVal fileName As String, ByVal saveAsFile As Boolean, ByVal fileFormat As String, ByVal stream As MemoryStream)
